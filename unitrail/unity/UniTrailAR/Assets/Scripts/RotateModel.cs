9096593621f1ce6fbb5ad5e399cd695aa6482198
FindingDorri <tho.le.nas@gmail.com>
2023-02-17 15:14:30 -0700

    test

---
+++ unitrail/unity/UniTrailAR/Assets/Scripts/RotateModel.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateModel : MonoBehaviour
+{
+    // Variables for rotating the object
+    public Transform objectToRotate;   
+    private Vector2 swipeDirection;
+    private float rotationSpeed = 0.2f;
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            // GET TOUCH 0
+            Touch touch0 = Input.GetTouch(0);
+            swipeDirection = Input.GetTouch(0).deltaPosition;
+            // APPLY ROTATION
+            if (touch0.phase == TouchPhase.Moved)
+            {
+                // objectToRotate.transform.Rotate(0f, touch0.deltaPosition.x, 0f);
+                objectToRotate.transform.Rotate(swipeDirection.y * rotationSpeed, -swipeDirection.x * rotationSpeed, 0, Space.World);
+            
+            }
+ 
+        }
+    }
+}
