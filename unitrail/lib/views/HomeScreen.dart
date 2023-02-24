2091b3835a5e59b22fb821a54d91eaaf81d8a58b
SHamil36 <seth.hamilton@aol.com>
2023-02-23 19:29:43 -0700

    got rid of the dropdown menu on homescreen, routed navigation for all current screens, stylized the navigation page

--- unitrail/lib/views/HomeScreen.dart
+++ unitrail/lib/views/HomeScreen.dart
@@ -1,7 +1,7 @@
 import 'package:expandable_bottom_bar/expandable_bottom_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
-import 'package:unitrail/views/NavigateScreen.dart';
+import 'package:unitrail/views/Components/GridDashboard.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,165 +9,29 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFa31621),
       appBar: AppBar(
           title: Image.asset(
-            "assets/images/logo.png",
+            "assets/images/team_logo.png",
             fit: BoxFit.contain,
             height: 5.h,
           ),
           toolbarHeight: 7.h,
           backgroundColor: const Color(0xFFa31621),
           actions: [
-            PopupMenuButton(
-              // add icon, by default "3 dot" icon
-              // icon: Icon(Icons.book)
-              itemBuilder: (context) {
-                return [
-                  const PopupMenuItem<int>(
-                    value: 0,
-                    child: Text("My Classes"),
-                  ),
-                  const PopupMenuItem<int>(
-                    value: 1,
-                    child: Text("Search Classrooms"),
-                  ),
-                  const PopupMenuItem<int>(
-                    value: 2,
-                    child: Text("Navigate"),
-                  ),
-                  const PopupMenuItem<int>(
-                      value: 3, child: Text("Calender (Beta)")),
-                  const PopupMenuItem<int>(value: 4, child: Text("Settings")),
-                  const PopupMenuItem<int>(value: 5, child: Text("AR Debug")),
-                ];
-              },
-              onSelected: (value) {
-                if (value == 0) {
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        title: const Text("My Classes"),
-                        shape: RoundedRectangleBorder(
-                            side: const BorderSide(
-                                color: Color(0xFFa31621), width: 5),
-                            borderRadius: BorderRadius.circular(20.0)),
-                        content: const Text(""),
-                        actions: [
-                          ElevatedButton(
-                              // ignore: sort_child_properties_last
-                              child: const Text("Ok"),
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              },
-                              style: ElevatedButton.styleFrom(
-                                  backgroundColor: const Color(0xff78c091),
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius:
-                                          BorderRadius.circular(20)))),
-                        ],
-                      );
-                    },
-                  );
-                }
-                if (value == 1) {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return NavigateScreen();
-                  }));
-                  //   showDialog(
-                  //     context: context,
-                  //     builder: (BuildContext context) {
-                  //       return AlertDialog(
-                  //         title: const Text("Search Classrooms"),
-                  //         shape: RoundedRectangleBorder(
-                  //             side: const BorderSide(
-                  //                 color: Color(0xFFa31621), width: 5),
-                  //             borderRadius: BorderRadius.circular(20.0)),
-                  //         content: const TextField(
-                  //           obscureText: true,
-                  //           decoration: InputDecoration(
-                  //             border: OutlineInputBorder(),
-                  //             labelText: 'Ex. AES 220',
-                  //           ),
-                  //         ),
-                  //         actions: [
-                  //           ElevatedButton(
-                  //               // ignore: sort_child_properties_last
-                  //               child: const Text("Add to My Classes"),
-                  //               onPressed: () {
-                  //                 Navigator.of(context).pop();
-                  //               },
-                  //               style: ElevatedButton.styleFrom(
-                  //                   backgroundColor: const Color(0xff78c091),
-                  //                   shape: RoundedRectangleBorder(
-                  //                       borderRadius:
-                  //                           BorderRadius.circular(20)))),
-                  //         ],
-                  //       );
-                  //     },
-                  //   );
-                  // }
-                  // if (value == 2) {
-                  //   showDialog(
-                  //     context: context,
-                  //     builder: (BuildContext context) {
-                  //       return AlertDialog(
-                  //           title: const Text("Navigate"),
-                  //           actionsAlignment: MainAxisAlignment.center,
-                  //           shape: RoundedRectangleBorder(
-                  //               side: const BorderSide(
-                  //                   color: Color(0xFFa31621), width: 5),
-                  //               borderRadius: BorderRadius.circular(20.0)),
-                  //           actions: [
-                  //             const TextField(
-                  //               obscureText: true,
-                  //               decoration: InputDecoration(
-                  //                 border: OutlineInputBorder(),
-                  //                 labelText: "Current Location",
-                  //               ),
-                  //             ),
-                  //             const TextField(
-                  //               obscureText: true,
-                  //               decoration: InputDecoration(
-                  //                 border: OutlineInputBorder(),
-                  //                 labelText: "Destination",
-                  //               ),
-                  //             ),
-                  //             ButtonBar(
-                  //               children: [
-                  //                 ElevatedButton(
-                  //                     // ignore: sort_child_properties_last
-                  //                     child: const Text("Cancel"),
-                  //                     onPressed: () {
-                  //                       Navigator.of(context).pop();
-                  //                     },
-                  //                     style: ElevatedButton.styleFrom(
-                  //                         backgroundColor:
-                  //                             const Color(0xFFa31621),
-                  //                         shape: RoundedRectangleBorder(
-                  //                             borderRadius:
-                  //                                 BorderRadius.circular(20)))),
-                  //                 ElevatedButton(
-                  //                     // ignore: sort_child_properties_last
-                  //                     child: const Text("Go"),
-                  //                     onPressed: () {
-                  //                       Navigator.of(context).pop();
-                  //                     },
-                  //                     style: ElevatedButton.styleFrom(
-                  //                         backgroundColor:
-                  //                             const Color(0xff78c091),
-                  //                         shape: RoundedRectangleBorder(
-                  //                             borderRadius:
-                  //                                 BorderRadius.circular(20)))),
-                  //               ],
-                  //             )
-                  //           ]);
-                  //     },
-                  //   );
-                }
-              },
-            ),
+            FloatingActionButton(
+              onPressed: () {},
+              // ignore: sort_child_properties_last
+              child: Icon(
+                Icons.logout_rounded,
+                color: Colors.black,
+              ),
+              backgroundColor: Color(0xFFfcf7f8),
+            )
           ]),
+      body: Column(
+        children: <Widget>[GridDashboard()],
+      ),
       //
       // Lets use docked FAB for handling state of sheet
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
@@ -210,9 +74,9 @@ class HomeScreen extends StatelessWidget {
               ],
             ),
           ),
-          elevation: 2,
-          backgroundColor: const Color(0xFFa31621),
-          foregroundColor: const Color(0xFFfcf7f8),
+          elevation: 1,
+          backgroundColor: const Color(0xFFfcf7f8),
+          foregroundColor: const Color(0xFFa31621),
           //
           //Set onPressed event to swap state of bottom bar
           onPressed: () => DefaultBottomBarController.of(context).swap(),
