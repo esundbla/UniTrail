58cfac6cda85796467c4f3dfdab15d301221e917
esundbla <69823637+esundbla@users.noreply.github.com>
2023-03-05 15:25:54 -0700

    mapping tool

--- unitrail/lib/BackEndTesting/Gmap.dart
+++ unitrail/lib/BackEndTesting/Gmap.dart
@@ -1,6 +1,8 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 import 'package:unitrail/views/Components/Building_LatLng.dart';
 
 final X011 = "AIzaSyDZAM1_YKEux_XQ6rcP7XPgcb-o_YBlVjs";
@@ -13,6 +15,7 @@ class GMapTest extends StatefulWidget {
 }
 
 class _GMapTest extends State<GMapTest> {
+  final Completer<GoogleMapController> _controller = Completer();
   late GoogleMapController mapController;
 
   LatLng _center = const LatLng(39.7439, -105.0047);
@@ -22,9 +25,27 @@ class _GMapTest extends State<GMapTest> {
   final LatLng boxSW = const LatLng(39.741209, -105.010962);
 
   List<LatLng> polyLineCords = [];
+  LocationData? currentLoc;
 
-  void getPolyPoits(ori, des) async {
+  void getCurrentLocation() async {
+    Location loc = Location();
+
+    loc.getLocation().then((value) => currentLoc = value);
+    GoogleMapController gMapCont = await _controller.future;
+
+    loc.onLocationChanged.listen((event) async {
+      currentLoc = event;
+
+      gMapCont.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+          zoom: 18.2, target: LatLng(event.latitude!, event.longitude!))));
+      await getPolyPoints(LatLng(currentLoc!.latitude!, currentLoc!.longitude!), dest);
+      setState(() {});
+    });
+  }
+
+  Future<void> getPolyPoints(ori, des) async {
     PolylinePoints plp = PolylinePoints();
+    polyLineCords = [];
 
     PolylineResult result = await plp.getRouteBetweenCoordinates(
         X011,
@@ -46,7 +67,8 @@ class _GMapTest extends State<GMapTest> {
 
   @override
   void initState() {
-    getPolyPoits(origin,dest);
+    getCurrentLocation();
+    getPolyPoints(origin, dest);
     super.initState();
   }
 
@@ -62,32 +84,42 @@ class _GMapTest extends State<GMapTest> {
               title: const Text('Maps Sample App'),
               elevation: 2,
             ),
-            body: GoogleMap(
-              onMapCreated: _onMapCreated,
-              initialCameraPosition: CameraPosition(
-                target: _center,
-                zoom: 15.0,
-              ),
-              polylines: {
-                Polyline(
-                  polylineId: PolylineId("Route"),
-                  points: polyLineCords,
-                  color: Colors.blue,
-                  width: 5,
-                )
-              },
-              cameraTargetBounds: CameraTargetBounds(
-                  LatLngBounds(northeast: boxNE, southwest: boxSW)),
-              markers: {
-                Marker(
-                  markerId: MarkerId("Current"),
-                  position: origin,
-                ),
-                Marker(
-                  markerId: MarkerId("Destination"),
-                  position: dest,
-                )
-              },
-            )));
+            body: currentLoc == null
+                ? const Center(child: const CircularProgressIndicator())
+                : GoogleMap(
+                    onMapCreated: (mapController) {
+                      _controller.complete(mapController);
+                    },
+                    initialCameraPosition: CameraPosition(
+                      target:
+                          LatLng(currentLoc!.latitude!, currentLoc!.longitude!),
+                      zoom: 15.0,
+                    ),
+                    polylines: {
+                      Polyline(
+                        polylineId: PolylineId("Route"),
+                        points: polyLineCords,
+                        color: Colors.blue,
+                        width: 5,
+                      )
+                    },
+                    cameraTargetBounds: CameraTargetBounds(
+                        LatLngBounds(northeast: boxNE, southwest: boxSW)),
+                    markers: {
+                      Marker(
+                        markerId: const MarkerId("Current"),
+                        position: LatLng(
+                            currentLoc!.latitude!, currentLoc!.longitude!),
+                      ),
+                      Marker(
+                        markerId: MarkerId("Origin"),
+                        position: origin,
+                      ),
+                      Marker(
+                        markerId: MarkerId("Destination"),
+                        position: dest,
+                      )
+                    },
+                  )));
   }
 }
