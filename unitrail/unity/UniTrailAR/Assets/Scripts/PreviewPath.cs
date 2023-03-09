a43561f43500aad50a960187c6f9320c50aa4482
FindingDorri <tho.le.nas@gmail.com>
2023-03-09 07:32:17 -0700

    Modified preview path script to display discontinuous lines on multiple floors

--- unitrail/unity/UniTrailAR/Assets/Scripts/PreviewPath.cs
+++ unitrail/unity/UniTrailAR/Assets/Scripts/PreviewPath.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement;
 
 public class PreviewPath : MonoBehaviour
 {
