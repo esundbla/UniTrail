c6accf5d8af1948a803eca157b548647b485633a
esundbla <69823637+esundbla@users.noreply.github.com>
2023-04-28 16:49:27 -0600

    clean out backtesting

--- unitrail/lib/main.dart
+++ unitrail/lib/main.dart
@@ -1,14 +1,9 @@
 import 'package:provider/provider.dart';
-import 'package:unitrail/BackEndTesting/read%20data/cam_scan.dart';
-import 'package:unitrail/views/welcome.dart';
+import 'package:unitrail/BE/read%20data/cam_scan.dart';
 import 'package:unitrail/views/Components/ics_tool.dart';
 import 'package:unitrail/views/Provider/event_provider.dart';
 import 'package:unitrail/views/Widgets/utils.dart';
-import 'package:unitrail/BackEndTesting/Gmap.dart';
 import 'package:unitrail/views/login_page.dart';
-import 'package:unitrail/views/welcome.dart';
-import 'package:unitrail/BackEndTesting/Testing.dart';
-import 'package:unitrail/views/Components/GridDashboard.dart';
 import 'package:flutter/material.dart';
 import 'package:unitrail/views/Components/event.dart';
 import 'package:flutter/services.dart';
@@ -18,7 +13,6 @@ import 'package:expandable_bottom_bar/expandable_bottom_bar.dart';
 import 'package:sizer/sizer.dart';
 import 'package:device_preview/device_preview.dart';
 import 'dart:async';
-import 'dart:io';
 import 'package:camera/camera.dart';
 
 List<Event>? events;
@@ -50,17 +44,16 @@ class MyApp extends StatelessWidget {
       return ChangeNotifierProvider(
         create: (context) => EventProvider(events: events!),
         child: MaterialApp(
-          scaffoldMessengerKey: Utils().messengerKey,
-          navigatorKey: navigatorKey,
-          title: 'UniTrail',
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
-          ),
-          home: DefaultBottomBarController(
-              child: Login())
-              //child: GridDashboard(schedule: [])),
-        ),
+            scaffoldMessengerKey: Utils().messengerKey,
+            navigatorKey: navigatorKey,
+            title: 'UniTrail',
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+            ),
+            home: DefaultBottomBarController(
+                child: Login()))
+                //child: camScan(camera: firstCamera!))),
       );
     });
   }
